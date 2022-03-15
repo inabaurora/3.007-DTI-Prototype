2f3180307044dd21a24ed05e51d2cbc2a7065070
inabaurora <91104559+inabaurora@users.noreply.github.com>
2022-03-15 22:01:02 +0800

    Add files via upload

---
+++ twinkle/twinkle.ino
@@ -0,0 +1,175 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 9
+#define Pixels 12
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(Pixels, PIN, NEO_GRB + NEO_KHZ800);
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
+
+float redStates[Pixels];
+float blueStates[Pixels];
+float greenStates[Pixels];
+float fadeRate = 0.96;
+
+
+class Strip
+{
+public:
+  uint8_t   effect;
+  uint8_t   effects;
+  uint16_t  effStep;
+  unsigned long effStart;
+  Adafruit_NeoPixel strip;
+  Strip(uint16_t leds, uint8_t pin, uint8_t toteffects, uint16_t striptype) : strip(leds, pin, striptype) {
+    effect = -1;
+    effects = toteffects;
+    Reset();
+  }
+  void Reset(){
+    effStep = 0;
+    effect = (effect + 1) % effects;
+    effStart = millis();
+  }
+};
+
+struct Loop
+{
+  uint8_t currentChild;
+  uint8_t childs;
+  bool timeBased;
+  uint16_t cycles;
+  uint16_t currentTime;
+  Loop(uint8_t totchilds, bool timebased, uint16_t tottime) {currentTime=0;currentChild=0;childs=totchilds;timeBased=timebased;cycles=tottime;}
+};
+
+Strip strip_0(107, 8, 107, NEO_GRB + NEO_KHZ800);
+struct Loop strip0loop0(1, false, 1);
+
+//[GLOBAL_VARIABLES]
+
+void setup() {
+
+  //Your setup here:
+
+  strip_0.strip.begin();
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+  
+  for(uint16_t l = 0; l < Pixels; l++) {
+    redStates[l] = 0;
+    greenStates[l] = 0;
+    blueStates[l] = 0;
+  }
+}
+
+void loop() {
+
+  //Your code here:
+
+  strips_loop();
+
+  if (random(20) == 1) {
+      uint16_t i = random(Pixels);
+      if (redStates[i] < 1 && greenStates[i] < 1 && blueStates[i] < 1) {
+        redStates[i] = random(256);
+        greenStates[i] = random(256);
+        blueStates[i] = random(256);
+      }
+    }
+    
+    for(uint16_t l = 0; l < Pixels; l++) {
+      if (redStates[l] > 1 || greenStates[l] > 1 || blueStates[l] > 1) {
+        strip.setPixelColor(l, redStates[l], greenStates[l], blueStates[l]);
+        
+        if (redStates[l] > 1) {
+          redStates[l] = redStates[l] * fadeRate;
+        } else {
+          redStates[l] = 0;
+        }
+        
+        if (greenStates[l] > 1) {
+          greenStates[l] = greenStates[l] * fadeRate;
+        } else {
+          greenStates[l] = 0;
+        }
+        
+        if (blueStates[l] > 1) {
+          blueStates[l] = blueStates[l] * fadeRate;
+        } else {
+          blueStates[l] = 0;
+        }
+        
+      } else {
+        strip.setPixelColor(l, 0, 0, 0);
+      }
+    }
+    strip.show();
+    delay(0.00001);
+  
+}
+
+void strips_loop() {
+  if(strip0_loop0() & 0x01)
+    strip_0.strip.show();
+}
+
+uint8_t strip0_loop0() {
+  uint8_t ret = 0x00;
+  switch(strip0loop0.currentChild) {
+    case 0: 
+           ret = strip0_loop0_eff0();break;
+  }
+  if(ret & 0x02) {
+    ret &= 0xfd;
+    if(strip0loop0.currentChild + 1 >= strip0loop0.childs) {
+      strip0loop0.currentChild = 0;
+      if(++strip0loop0.currentTime >= strip0loop0.cycles) {strip0loop0.currentTime = 0; ret |= 0x02;}
+    }
+    else {
+      strip0loop0.currentChild++;
+    }
+  };
+  return ret;
+}
+
+uint8_t strip0_loop0_eff0() {
+    // Strip ID: 0 - Effect: Rainbow - LEDS: 107
+    // Steps: 107 - Delay: 20
+    // Colors: 3 (255.0.0, 0.255.0, 0.0.255)
+    // Options: rainbowlen=107, toLeft=true, 
+  if(millis() - strip_0.effStart < 20 * (strip_0.effStep)) return 0x00;
+  float factor1, factor2;
+  uint16_t ind;
+  for(uint16_t j=0;j<107;j++) {
+    ind = strip_0.effStep + j * 1;
+    switch((int)((ind % 107) / 36)) {
+      case 0: factor1 = 1.0 - ((float)(ind % 107 - 0 * 36) / 36);
+              factor2 = (float)((int)(ind - 0) % 107) / 36;
+              strip_0.strip.setPixelColor(j, 255 * factor1 + 0 * factor2, 0 * factor1 + 255 * factor2, 0 * factor1 + 0 * factor2);
+              break;
+      case 1: factor1 = 1.0 - ((float)(ind % 107 - 1 * 36) / 36);
+              factor2 = (float)((int)(ind - 36) % 107) / 36;
+              strip_0.strip.setPixelColor(j, 0 * factor1 + 0 * factor2, 255 * factor1 + 0 * factor2, 0 * factor1 + 255 * factor2);
+              break;
+      case 2: factor1 = 1.0 - ((float)(ind % 107 - 2 * 36) / 36);
+              factor2 = (float)((int)(ind - 72) % 107) / 36;
+              strip_0.strip.setPixelColor(j, 0 * factor1 + 255 * factor2, 0 * factor1 + 0 * factor2, 255 * factor1 + 0 * factor2);
+              break;
+    }
+  }
+  if(strip_0.effStep >= 107) {strip_0.Reset(); return 0x03; }
+  else strip_0.effStep++;
+  return 0x01;
+}
